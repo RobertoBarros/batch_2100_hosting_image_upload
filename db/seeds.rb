b4c91984b6131764fbc8107d08b5720b1fb07811
Roberto Barros <roberto.f.barros@gmail.com>
2025-08-29 10:29:19 -0300

    upload de fotos do usuário

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,8 @@
-Article.create!(title: "My First Article", body: "Hello, this is my first article here!!")
+require "open-uri"
+
+url = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/9b/05/c5/restaurante-rick-s.jpg"
+file = URI.parse(url).open
+
+article = Article.new(title: "My First Article", body: "Hello, this is my first article here!!")
+article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+article.save!
